6b96d1d666889d5d09d5d593e449fc6d1ea51225
ArnabTo <arnabs929@gmail.com>
2024-08-01 23:47:06 +0600

    added features section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}", './index.html'],
   theme: {
     extend: {
-     
+     fontSize:{
+      '4.5xl': ['2.62rem'],
+     },
+     padding:{
+      '22' : ['88px']
+     }
     },
   },
   plugins: [],
