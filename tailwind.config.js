7633577653c95386df3a6bb9b15b7d5b5e060191
ArnabTo <arnabs929@gmail.com>
2024-08-04 11:42:25 +0600

    updated ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,27 @@ module.exports = {
   content: ["./src/**/*.{html,js}", './index.html'],
   theme: {
     extend: {
-     fontSize:{
-      '4.5xl': ['2.62rem'],
-      'xxs' : ['0.5rem']
-     },
-     padding:{
-      '22' : ['88px']
-     }
+      width: {
+        '92' : '23rem'
+      },
+      height:{
+        '92' : '23rem'
+      },
+      fontSize: {
+        '4.5xl': ['2.62rem'],
+        'xxs': ['0.5rem']
+      },fontFamily:{
+        'poppins' : ["Poppins", "sans-serif"]
+      },
+      padding: {
+        '22': ['88px']
+      },
+      spacing: {
+        '144': '36rem'
+      },
+      blur: {
+        '42': '10.5rem'
+      }
     },
   },
   plugins: [],
